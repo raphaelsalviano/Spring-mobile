749ea3b21a082de021bfcd18e29b0a7f546d1261
Rodrigo Fujioka <rodrigofujioka@gmail.com>
2018-07-27 21:10:46 -0300

    Inicializando Pós mobile

---
+++ src/main/java/br/unipe/pos/mobile/api/PersonApplication.java
@@ -0,0 +1,14 @@
+package br.unipe.pos.mobile.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class PersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonApplication.class, args);
+	}
+}
